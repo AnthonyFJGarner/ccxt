180b69d3a0eb372a02a1c4e545c2efed76a0278d
AnthonyFJGarner <anthonyfjgarner@gmail.com>
2025-01-26 16:38:46 +0000

    More cccxt master changes

--- examples/test.py
+++ examples/test.py
@@ -6,10 +6,10 @@ exchange_class = getattr(ccxt, exchange_id)
 exchange = exchange_class()
 exchange.load_markets()
 instrument = exchange.markets[symbol]['id']
-since = exchange.parse8601 ('2025-01-20T00:00:00')
+since = exchange.parse8601 ('2019-05-01T00:00:00')-1
 until = exchange.milliseconds()
 limit = 1000
 funding_rate_history = exchange.fetch_funding_rate_history(symbol, since, limit, params={'paginate': True,'until': until})
 print(funding_rate_history)
-print("Done")
+
 
